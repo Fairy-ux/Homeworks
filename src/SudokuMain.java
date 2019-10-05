c38ba52285cf7dbb6c429f5bda40d020294123e3
Fairy-ux <56143102+Fairy-ux@users.noreply.github.com>
2019-10-04 22:28:43 -0700

    Delete SudokuMain.java

--- src/SudokuMain.java
+++
@@ -1,9 +0,0 @@
-import java.io.FileNotFoundException;
-
-public class SudokuMain {
-    public static void main(String[] args) throws FileNotFoundException {
-        String filePath = "sudoku.sdkx";
-        SudokuBoard sudoku = new SudokuBoard(filePath);
-        System.out.println(sudoku);
-    }
-}

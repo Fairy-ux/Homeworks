9a0cc74277fc6e869137f258497ad8d46b9eca10
Fairy-ux <56143102+Fairy-ux@users.noreply.github.com>
2019-10-16 13:50:18 -0700

    Update BoardReaderFactory.java

--- Homework2/src/main/java/BoardReaderFactory.java
+++ Homework2/src/main/java/BoardReaderFactory.java
@@ -3,10 +3,10 @@ package main.java;
 public class BoardReaderFactory {
     public static BoardReader getReader(String format) {
         if (format.equalsIgnoreCase("sdk")) {
-            return new SDKReader();
+            return new SDKReader("src/main/resources/board.sdkx");
         }
         else if (format.equalsIgnoreCase("ss")) {
-            return new SSReader();
+            return new SSReader("src/main/resources/board.ss");
         }
         throw new UnsupportedOperationException("Reader for format " + format
         + " is not supported");
